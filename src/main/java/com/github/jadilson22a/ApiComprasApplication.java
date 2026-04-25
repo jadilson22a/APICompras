c1a4cc8ed52e9512beffe6bc6241e29037b3cf7f
Jadilson22a <jadilsonalves777.ma@outlook.com>
2026-04-25 00:05:28 -0300

    configurando projeto APICompras

---
+++ src/main/java/com/github/jadilson22a/ApiComprasApplication.java
@@ -0,0 +1,13 @@
+package com.github.jadilson22a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jadilson22a/ApiComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jadilson22a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
